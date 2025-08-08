ff0a23b4ab7f57894d6469e1a5d3e91fb997cdd0
Suyash Shinde <suyashshinde5617@gmail.com>
2025-08-08 22:22:33 +0530

    Add files via upload

---
+++ program8.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+// User is going to enter only positve inputs
+
+// Algirithm
+/*
+    START
+        Accept one number as No
+        Divide that number No by 2
+        If remainder is 0
+        then display the result as Even number
+        otherwise display the result as Odd number
+    STOP
+*/
+
+////////////////////////////////////////////////////////////
+//
+//  Function Name :     CheckEvenOdd
+//  Description :       Used to check whether number is even or odd
+//  Input :             Integer
+//  Output :            Boolean
+//  Author :            Piyush Manohar Khairnar
+//  Date :              16/04/2024
+//
+////////////////////////////////////////////////////////////
+
+bool CheckEvenOdd(unsigned int iNo)
+{
+    if((iNo % 2) == 0)
+    {   return true;    }
+    else
+    {   return false;   }
+}
+
+int main()
+{
+    unsigned int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is Even number\n",iValue);
+    }
+    else 
+    {
+        printf("%d is Odd number\n",iValue);
+    }
+    return 0;
+}
+
+
+// 4. Write typescript program which cantains one function named as chkPrime.that fuction shoud
+// accept one number and it should return true if the given number is prime and otherwise return false.
+//Input : 21
+//Output : It is prime      
